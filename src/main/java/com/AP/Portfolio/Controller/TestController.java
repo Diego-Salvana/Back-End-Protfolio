0bb03296a45f781dc43d70065b756a77d4453bdc
Diego Salvañá <diegofsalvana@gmail.com>
2022-05-24 15:22:01 -0300

    agregado crossorigin

--- src/main/java/com/AP/Portfolio/Controller/TestController.java
+++ src/main/java/com/AP/Portfolio/Controller/TestController.java
@@ -2,12 +2,14 @@ package com.AP.Portfolio.Controller;
 
 import java.time.Instant;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://heroku-springboot-portfolio.herokuapp.com/")
 @RequestMapping("/api/v1.0")
 public class TestController {
 
