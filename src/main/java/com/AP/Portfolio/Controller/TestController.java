c86331d2766aa0736dc079639ed2183a553e9081
Diego Salvañá <diegofsalvana@gmail.com>
2022-06-09 17:39:40 -0300

    prueba test-controler

--- src/main/java/com/AP/Portfolio/Controller/TestController.java
+++ src/main/java/com/AP/Portfolio/Controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
-    @GetMapping("/time")
+    @GetMapping("/time/now")
     @ResponseStatus(HttpStatus.OK)
     public String getCurrentTime() {
 
